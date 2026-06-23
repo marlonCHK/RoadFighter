429e4dac1dc0e144af368c74ef8e53aba184ae60
marlonCHK <marlon.kruchinski.chaves@gmail.com>
2026-06-23 15:47:51 -0300

    add entity_factory

---
+++ code/entity_factory.py
@@ -0,0 +1,24 @@
+import random
+
+from code.inimigo import Inimigo
+
+
+class EntityFactory:
+
+    @staticmethod
+    def criar_inimigo(velocidade):
+
+        lado = random.choice(
+            ["esquerda", "direita"]
+        )
+
+        if lado == "esquerda":
+            posicao = -100
+        else:
+            posicao = 100
+
+        return Inimigo(
+            posicao_x=posicao,
+            direcao=lado,
+            velocidade=velocidade
+        )
